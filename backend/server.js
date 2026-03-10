6db8332ce071e00802ca3611e61d6f7e5d1ff9c4
Maffan <maffa@example.com>
2026-03-10 05:48:44 +0500

    chore: configure for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -30,8 +30,11 @@ app.use('/api/courses', courseRoutes);
 
 
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
